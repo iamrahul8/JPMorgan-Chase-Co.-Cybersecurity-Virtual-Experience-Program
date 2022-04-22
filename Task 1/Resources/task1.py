1d0d1317057d76dbeefa1f512d569829f4ee229d
iamrahul8 <rkgkumar072@gmail.com>
2022-04-23 04:03:06 +0530

    Task1 added all

---
+++ Task 1/Resources/task1.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def exercise_0(file):
+    pass
+
+def exercise_1(df):
+    pass
+
+def exercise_2(df, k):
+    pass
+
+def exercise_3(df, k):
+    pass
+
+def exercise_4(df):
+    pass
+
+def exercise_5(df):
+    pass
+
+def exercise_6(df):
+    pass
+
+def exercise_7(df):
+    pass
+
+def visual_1(df):
+    pass
+
+def visual_2(df):
+    pass
+
+def exercise_custom(df):
+    pass
+    
+def visual_custom(df):
+    pass
